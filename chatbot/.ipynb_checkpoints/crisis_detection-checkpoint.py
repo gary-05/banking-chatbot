b686aea3542d2889b02eaf570e870c4d1d14c6c8
Garima <garimasingh0529@gmail.com>
2026-02-10 22:50:21 +0530

    Delete chatbot/.ipynb_checkpoints/crisis_detection-checkpoint.py

--- chatbot/.ipynb_checkpoints/crisis_detection-checkpoint.py
+++
@@ -1,8 +0,0 @@
-
-CRISIS_KEYWORDS = ["suicide", "kill myself", "end my life"]
-
-def detect_crisis(text):
-    for word in CRISIS_KEYWORDS:
-        if word in text.lower():
-            return True
-    return False
